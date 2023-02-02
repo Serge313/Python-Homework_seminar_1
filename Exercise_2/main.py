76ab720bbec275767572201b943cf3e2238a433e
Serge Konyakhin <sergekonyakhin@gmail.com>
2023-02-02 15:53:01 +0300

    Exercise_2 modified

--- Exercise_2/main.py
+++ Exercise_2/main.py
@@ -1,12 +1,17 @@
-def each_person_birds(s):
-    boys = s // 3
-    a = boys // 2
-    b = boys * 2
-    c = a
-    return a, b, c
+"""Задача 2: Петя, Катя и Сережа делают из бумаги журавликов. Вместе они сделали S журавликов. Сколько журавликов сделал каждый ребенок, если известно, что Петя и Сережа сделали одинаковое количество журавликов, а Катя сделала в два раза больше журавликов, чем Петя и Сережа вместе?"""
+
+
+def each_person_birds(summary):
+    """The method counts the number of crane birds made by each person"""
+    boys = summary // 3
+    first_boy = boys // 2
+    girl = boys * 2
+    second_boy = first_boy
+    return first_boy, girl, second_boy
 
 
 def testing_each_person_birds(test_summary_of_birds=60):
+    """The method tests each_person_birds method"""
     print("Testing of the \"each_person_birds\" method has been launched...")
     expected = (10, 40, 10)
     actual = each_person_birds(test_summary_of_birds)
@@ -20,6 +25,7 @@ def testing_each_person_birds(test_summary_of_birds=60):
 testing_each_person_birds()
 
 print()
+
 number_of_crane_birds = int(input("Enter number of crane birds: "))
 birds = each_person_birds(number_of_crane_birds)
 is_even = number_of_crane_birds % 2 == 0 and number_of_crane_birds % 3 == 0
