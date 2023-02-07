971003f36374ae564fc52eed68e2c4a9961197cb
Serge Konyakhin <sergekonyakhin@gmail.com>
2023-02-07 19:55:59 +0300

    Exercise_1 modified

--- Exercise_1/main.py
+++ Exercise_1/main.py
@@ -1,17 +1,48 @@
-three_digit_number = int(input("Enter your three-digit number: "))
+"""Задача 1: Найдите сумму цифр трехзначного числа"""
+
+import sys
 
 
-def find_digits(number):
+def find_sum_of_digits(number):
+    """The method separates digits and finds their sum"""
     if number == 0:
         return number
     else:
-        return number % 10 + find_digits(number // 10)
+        return number % 10 + find_sum_of_digits(number // 10)
 
 
-is_three_digit_number = 99 < three_digit_number < 1000
+def testing_find_sum_of_digits(test_number=123):
+    """The method tests find_sum_of_digits method"""
+    print("Testing of the \"find_sum_of_digits\" method has been launched...")
+    expected = 6
+    actual = find_sum_of_digits(test_number)
+    is_equal = expected == actual
+    if is_equal:
+        print("Test completed successfully!")
+    else:
+        print("Error! Need to fix the method!")
 
-if is_three_digit_number:
-    summary = find_digits(three_digit_number)
-    print(summary)
-else:
-    print("Please enter a THREE-DIGIT number!")
+
+testing_find_sum_of_digits()
+print()
+
+
+class InvalidNumber(Exception):
+    pass
+
+
+try:
+    three_digit_number = int(input("Enter your three-digit number: "))
+    if three_digit_number < 99 or three_digit_number > 1000:
+        raise InvalidNumber("The number must be three digits!")
+except InvalidNumber as ex:
+    print(ex)
+    sys.exit()
+except Exception as ex:
+    print(f"Error! {ex}")
+    sys.exit()
+
+
+print()
+summary = find_sum_of_digits(three_digit_number)
+print(f"{three_digit_number} -> {summary}")
